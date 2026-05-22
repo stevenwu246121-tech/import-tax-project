a5823cad02d1351251f580168b760b91ee01d9ca
DESKTOP-83TOSSP\user <stevenwu246121@gmail.com>
2026-05-22 09:49:46 +0800

    Add HS Code search feature

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 
-import { ProductsComponent } from './@components/products/products.component';
+import { ProductsComponent }
+from './@components/products/products.component';
+
+import { HsCodeComponent }
+from './@components/hs-code/hs-code.component';
 
 export const routes: Routes = [
-  {
-    path: '',
-    component: ProductsComponent
-  }
+  {path: '',component: ProductsComponent},
+
+  {path: 'hs-code',component: HsCodeComponent}
 ];
